49afb43119bc85bdf311cfd128284605bf8b9f51
Joclelson Rodrigues <jocleelson.159@hotmail.com>
2025-05-19 15:59:03 -0300

    feat: :sparkles: Criação do modulo notifications

--- ms-notification/src/app.module.ts
+++ ms-notification/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { NotificationsModule } from './modules/notifications/notifications.module';
+import { PubsubModule } from './modules/pubsub/pubsub.module';
+import { UsersModule } from './modules/users/users.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), NotificationsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    NotificationsModule,
+    PubsubModule,
+    UsersModule,
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
--- ms-notification/src/main.ts
+++ ms-notification/src/main.ts
@@ -3,16 +3,19 @@ import { AppModule } from './app.module';
 import { swaggerConfig } from './common/config/swagger.config';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { redisConfig } from './common/config/redis.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  app.useGlobalPipes(new ValidationPipe());
 
+  app.useGlobalPipes(new ValidationPipe());
   swaggerConfig(app, 'development');
 
   const PORT = configService.get<number>('PORT') || 3001;
   await app.listen(PORT);
+
+  await redisConfig(configService);
 }
 
 bootstrap();
